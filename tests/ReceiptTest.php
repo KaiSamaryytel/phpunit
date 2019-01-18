f81ae2287804a36d31f464b3a4ed198cdc05ff0d
KaiSamaryytel <kai.koppel@khk.ee>
2019-01-18 15:05:31 +0400

    Adding xml file

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -1,19 +1,40 @@
 <?php
 namespace TDD\Test;
-require 'vendor\autoload.php';
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
 
 use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
+/*Siin luuakse Receipt*/
 class ReceiptTest extends TestCase {
-    /**
-     *
-     */
-    public function testTotal(){
-        $Receipt = new Receipt();
-        $this-> assertEquals(15,
-            $Receipt->total([0,2,5,8]),
-            'When summing the total should equal 15 '
+    public function setUp() {
+        $this->Receipt = new Receipt();
+    }
+
+    public function tearDown() {
+        unset($this->Receipt);
+    }
+
+    /*Siin võrreldakse, kas input ja output on võrdsed. Tuleb teade*/
+
+    public function testTotal() {
+        $input = [0,2,5,8];
+        $output = $this->Receipt->total($input);
+        $this->assertEquals(
+            15,
+            $output,
+            'When summing the total should equal 15'
+        );
+    }
+/*Siin lisatakse summale maksuosa ja antakse. Tuleb teade summa kohta. */
+    public function testTax() {
+        $inputAmount = 10.00;
+        $taxInput = 0.10;
+        $output = $this->Receipt->tax($inputAmount, $taxInput);
+        $this->assertEquals(
+            1.00,
+            $output,
+            'The tax calculation should equal 1.00'
         );
     }
 }
